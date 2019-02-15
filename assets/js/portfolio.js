3bcf525c0f3dc18c168756b7cff5f51b96be9271
gracemiller23 <gracemiller0323@gmail.com>
2019-02-15 13:46:53 -0700

    refactor portfolio.js so that a display row hides when a different row is activated

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,118 +1,122 @@
 //stores information to display
 const comingContent = "<h3> Sample Coming Soon </h3> <p> I'm currently working to organize and load samples of my development work to the site. Thank you for your patience!</p>";
-const comingImage ="https://via.placeholder.com/350x150";
+const comingImage = "https://via.placeholder.com/350x150";
+
 const portfolio = {
-    
-    html:{
-        content:`<p>I started learning HTML as a senior in college, when I built my first website for my senior portfolio. While the website didn't last, the knowledge did. When I returned to coding throughout my writing and editing career, I kept up with evolving standards for HTML5.`,
+
+    html: {
+        content: `<p>I started learning HTML as a senior in college, when I built my first website for my senior portfolio. While the website didn't last, the knowledge did. When I returned to coding throughout my writing and editing career, I kept up with evolving standards for HTML5.`,
         image: `${comingImage}`,
         percent: "100"
     },
-    css:{
-        content:`${comingContent}`,
+    css: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "90"
     },
-    javascript:{
-        content:`${comingContent}`,
+    javascript: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "70"
     },
-    jquery:{
-        content:`${comingContent}`,
+    jquery: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "70"
     },
-    bootstrap:{
-        content:`${comingContent}`,
+    bootstrap: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "100"
     },
-    handlebars:{
-        content:`${comingContent}`,
+    handlebars: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "60"
     },
-    responsivedesign:{
-        content:`${comingContent}`,
+    responsivedesign: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "100"
     },
-    mysql:{
-        content:`${comingContent}`,
+    mysql: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "75"
     },
-    mongodb:{
-        content:`${comingContent}`,
+    mongodb: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "75"
     },
-    sequelize:{
-        content:`${comingContent}`,
+    sequelize: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "75"
     },
-    mongoose:{
-        content:`${comingContent}`,
+    mongoose: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "75"
     },
-    nodejs:{
-        content:`${comingContent}`,
+    nodejs: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "50"
     },
-    apis:{
-        content:`${comingContent}`,
+    apis: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "90"
     },
-    expressserver:{
-        content:`${comingContent}`,
+    expressserver: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "40"
     },
-    userauthentication:{
-        content:`${comingContent}`,
+    userauthentication: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "35"
     },
-    react:{
-        content:`${comingContent}`,
+    react: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
         percent: "45"
     },
-    php:{
-        content:`${comingContent}`,
+    php: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
-        percent:"20"
+        percent: "20"
     },
-    wordpress:{
-        content:`${comingContent}`,
+    wordpress: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
-        percent:"20"
+        percent: "20"
     },
-    sass:{
-        content:`${comingContent}`,
+    sass: {
+        content: `${comingContent}`,
         image: `${comingImage}`,
-        percent:"20"
+        percent: "20"
     }
 };
-//the idNum argument below allows you to use the same function to affect different divs with different buttons on the same page
+//the idNum argument below allows you to use the same function to have multiple button group rows with different display rows.
 function expandMe(idNum) {
     //combine an identifying number with base id-names for the containing
-    //div, as well as the two columns
+    //div, as well as the display two columns
     let idName = `display-row${idNum}`;
     let colContentId = `display-col-content${idNum}`;
     let colImgId = `display-col-img${idNum}`;
-    console.log(event.target);
+
     //store the topic that each button should call in its value and use it here
     let retrieve = event.target.value;
-    let idElement = document.getElementById(idName);
+
+    //gather information about the row to later determine if it needs to be shown and if another row needs to be hidden
+    let idElement = document.querySelector(`#${idName}`);
+    let targetClassName = idElement.className;
     let displayValue = idElement.style.display;
     let dataCurrentValue = idElement.getAttribute("data-current");
-   
+
     //if the display div is already visible for this button, hide it on second click
     if (displayValue === "flex" && retrieve === dataCurrentValue) {
         console.log("I went with the first option...")
@@ -121,25 +125,62 @@ function expandMe(idNum) {
         document.getElementById(colImgId).innerHTML = "";
         let buttons = document.getElementsByTagName("button");
         event.target.setAttribute("id", "");
+
     //else show the display div with the matching content from the js object
     } else {
-        //add code to remove current-button id from other element get element with that id and remove it
+
+        //remove current-button id from another button, if there is one with that id
         let prevButton = document.getElementById("current-button");
-        console.log(prevButton);
-        if(prevButton !== null){
+
+        if (prevButton !== null) {
             prevButton.setAttribute("id", "");
+
         }
+
+        //set the targeted button's id to current-button
         event.target.setAttribute("id", "current-button");
+
+        //make sure correct display row is showing, if not, close previous row
+        if (targetClassName !== "row current-row") {
+
+            //find the row that has the class "current-row"
+            let prevRow = document.querySelector(`.current-row`);
+            console.log(prevRow);
+
+            //get that row's class names and split them into an array so the new class name can preserve "row"
+            let oldRowClass = prevRow.className;
+            
+            let oldRowClassArray = oldRowClass.split(" ");
+            let removableClassIndex = oldRowClassArray.indexOf("current-row");
+            oldRowClassArray.splice(removableClassIndex, 1);
+            let newRowClass = oldRowClassArray.join(" ");
+
+            //set the row's class to exclude "current-row" and hide the row
+            prevRow.setAttribute("style", "display:none");
+            prevRow.setAttribute("class", newRowClass);
+
+
+        }
+
+        //after old display row is hidden, display new row
         idElement.setAttribute("style", "display:flex;");
         idElement.setAttribute("data-current", retrieve);
-        let htmlContent= portfolio[retrieve].content;
-        htmlContent += `<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow=${portfolio[retrieve].percent} aria-valuemin="0" aria-valuemax="100" style="width: ${portfolio[retrieve].percent}%"></div></div>`
+        //get the element's classes and check to see if they already include current-row, so that they aren't altered when adding current-row.
+        let elementClass = idElement.className;
+            if(!elementClass.includes("current-row")){
+            elementClass += " current-row"
+            }
+        idElement.setAttribute("class", elementClass);
 
+        //change the content of the display row using info stored in array
+        let htmlContent = `<div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow=${portfolio[retrieve].percent} aria-valuemin="0" aria-valuemax="100" style="width: ${portfolio[retrieve].percent}%"></div></div>`;
+        
+        htmlContent += portfolio[retrieve].content;
         document.getElementById(colContentId).innerHTML =
-             htmlContent;
+            htmlContent;
         document.getElementById(colImgId).innerHTML =
             "<img src=" + portfolio[retrieve].image + " alt='alt' style='height: 100px; width:auto;'>";
-            idElement.scrollIntoView(false);
-        }
+        idElement.scrollIntoView(false);
+    }
 
 }
